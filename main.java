7cdeee934e9b993b954447bbfc0be8111d7a12f1
Musthaffa Shaik <musthaffa.shaik@planview.com>
2024-07-17 20:54:33 +0530

    insertion sort added by musthaffa

--- main.java
+++ main.java
@@ -54,6 +54,30 @@ public class SortingAlgorithms {
     
     public static void insertionSort() {
         System.out.println("Insertion Sort Algorithm");
-        // Implementation of Insertion Sort
+        int[] arr = {12, 11, 13, 5, 6};
+
+        System.out.println("Array before sorting:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+            }
+
+        System.out.println("\nSorting using Insertion Sort:");
+        insertionSort(arr);
+
+        System.out.println("\nArray after sorting:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+            }
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
     }
 }
